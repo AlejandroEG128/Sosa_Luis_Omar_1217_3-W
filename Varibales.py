40c75ab73300681dd09c0ed5ed93658f27ddb7eb
AlejandroEG128 <alejandro.escobedo@cbtis128.edu.mx>
2024-10-02 15:16:35 -0600

    Add files via upload

---
+++ Varibales.py
@@ -0,0 +1,33 @@
+
+print("hola mundo") 
+""""
+este comentario es para avisar que el print que esta en la parte
+de arriba es a que va a aparecer 
+en la pantalla principal
+"""
+""""
+"""
+x = 10 
+y = "mi calificacion final"
+
+#este es un prgrama de variables que nos dice el valor de A y de a
+
+print(" ")#para dejar un espacio al momento de ejecutar 
+print("Sosa Luis Omar: Mi practica de variables")
+print(" ")#para dejar un espacio al momento de ejecutar 
+
+print(y) #este valor es una cadena de caracteres 
+print(x) #este valor es numerico entero                 #SOSA LUIS OMAR 3-W
+
+A = int(4) #nos dice o asegura que A es o sera igual a 4 
+a =str("cuatro") #muestrame que a sera igual a 4 
+
+#print(type(a))
+#print(type(b))           
+#print(type(c))
+
+print(A) #muestra en pantalla la letra A 
+print(a) #muestra en pantalla la letra a
+#print(c) 
+
+print(" ") #para dejar un espacio al momento de ejecutar 
